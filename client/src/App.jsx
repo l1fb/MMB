b22e29d944ae462b9056f040069918582568a16d
l1fb <davchung5@gmail.com>
2018-08-31 15:31:20 -0700

    configures basic structures for reactjs and sass implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 
+import "./stylesheets/main.scss";
+
 class App extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render(){
-        return(
-            <div className="app">
-                Hello!
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="app">
+        Helo
+      </div >
+    );
+  }
 }
 
 export default App;
