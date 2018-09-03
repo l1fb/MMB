308c0d60def17eba1673adccc89e4b8a96aaa443
l1fb <davchung5@gmail.com>
2018-09-02 20:51:20 -0700

    [reverse]successfully obtains the data from api server

--- api_side/index.js
+++ api_side/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/routes.js");
+const cors = require("cors");
+const corsOption = {
+    origin: 'http://localhost:5000',
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type',
+    optionsSuccessStatus: 200
+}
 
 const apiApp = express();
 const port = 3000;
 
+
+apiApp.use(cors(corsOption));
 apiApp.use(bodyParser.urlencoded({ extended: true }));
 apiApp.use(bodyParser.json());
 
--- api_side/routes/controllers/ReverseController.js
+++ api_side/routes/controllers/ReverseController.js
@@ -1,13 +1,14 @@
 const reverseCommands = {
     reverseStr: ((req, res) => {
-        const originStr = req.body.str;
+        const originStr = req.body.data;
+        console.log(originStr)
         let reversedStr = "";
 
         //O(n) time complexity
         for (let i = originStr.length - 1; i >= 0; i--) {
             reversedStr += originStr[i];
         }
-        res.send(201, reversedStr);
+        res.status(201).send(reversedStr);
     })
 }
 
